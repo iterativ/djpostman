853119b6f063c3a02ed3b5eac53efdd79cae88a1
maersu <maersu@judges.ch>
2012-03-19 15:39:08 +0100

    first mailer version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-deliverer',
+    version='0.2',
+    description="a asynchron mail agent",
+    author='Marcel Eyer',
+    author_email='marcel.eyer@iterativ.ch',
+    url='https://github.com/iterativ/django-deliverer',
+    packages=['django-deliverer'],
+    zip_safe=False,
+)
