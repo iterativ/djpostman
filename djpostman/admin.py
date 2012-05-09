530a2d178843c0feaa78d41a388c788ff7ea8102
maersu <me@maersu.ch>
2012-05-09 14:03:43 +0200

    tasks

--- djpostman/admin.py
+++ djpostman/admin.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+#
+# ITerativ GmbH
+# http://www.iterativ.ch/
+#
+# Copyright (c) 2012 ITerativ GmbH. All rights reserved.
+#
 
 from django.contrib import admin
 from djpostman.models import Message
--- djpostman/sender.py
+++ djpostman/sender.py
@@ -5,20 +5,6 @@
 #
 # Copyright (c) 2012 ITerativ GmbH. All rights reserved.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-#
 # Created on Mar 19, 2012
 # @author: github.com/maersu
 
@@ -85,7 +71,6 @@ def send_multi_mail(subject, content, recipient_list,
     h.ignore_images = True
     h.ignore_emphasis = True
     
-    #email = msg.email
     email = EmailMultiAlternatives(force_unicode(subject), 
                                    force_unicode(h.handle(strip_empty_tags(strip_tags(content, ['img', 'script', 'span'])))), 
                                    from_email, 
@@ -95,6 +80,9 @@ def send_multi_mail(subject, content, recipient_list,
         msg.email = email
         msg.save()
     
-    send_mail_task.delay(email)
+    if 'djcelery' in settings.INSTALLED_APPS:
+        send_mail_task.delay(email)
+    else:
+        send_mail_task(email)
     
     return 1
