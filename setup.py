ef54c119a969d79bc45f07072527e54ec37e4d19
maersu <me@maersu.ch>
2013-03-26 15:24:05 +0100

    show bcc fields

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ for scheme in INSTALL_SCHEMES.values():
 
 setup(
     name='djpostman',
-    version='0.4.17',
+    version='0.4.18',
     description="a asynchron mail agent",
     author='Marcel Eyer',
     author_email='marcel.eyer@iterativ.ch',
