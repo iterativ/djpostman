807cd380283148cac87b8132012ac5c1d0b43d37
maersu <me@maersu.ch>
2013-06-18 21:35:46 +0200

    change version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ for scheme in INSTALL_SCHEMES.values():
 
 setup(
     name='djpostman',
-    version='0.4.19',
+    version='0.5.00',
     description="a asynchron mail agent",
     author='Marcel Eyer',
     author_email='marcel.eyer@iterativ.ch',
