6983871935de0d325078b7a633e3a4161c2881b0
Daniel Egger <daniel.egger@gmail.com>
2014-05-19 15:50:18 +0200

    dependency version update due to celery problems

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ for scheme in INSTALL_SCHEMES.values():
 
 setup(
     name='djpostman',
-    version='0.5.1',
+    version='0.5.2',
     description="a asynchron mail agent",
     author='Marcel Eyer',
     author_email='marcel.eyer@iterativ.ch',
@@ -51,7 +51,7 @@ setup(
         'Celery==3.0.9',
         'django-celery==3.0.9',
         'textile==2.1.5',
-        'kombu==2.4.0' # http://stackoverflow.com/questions/12115692/celery-error-no-such-transport-amqp
+        'kombu==2.5.16' # http://stackoverflow.com/questions/12115692/celery-error-no-such-transport-amqp
     ],
     dependency_links = [
         'http://github.com/aaronsw/html2text/tarball/master#egg=html2text-3.200.3'
